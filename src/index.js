21522a81dce50770c5a5c1089557b5c73397b230
SUMED  C.KALE <sumit.kale847@gmail.com>
2021-12-11 14:46:11 +0530

    CSS styling and importing in css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name = "Sumed";
+const img1 ="https://picsum.photos/200/300";
+const img2 ="https://picsum.photos/250/300";
+const img3 ="https://picsum.photos/300/300";
+
+const links ="https://www.picsum.com";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1 className='heading'>My Name is {name}</h1>
+    <div className="img_div">
+    <img src = {img1} alt="randomImages" />
+    <img src = {img2} alt="randomImages" />
+    <a href= {links} target="_blank">
+    <img src = {img3} alt="randomImages" />
+     </a>
+     </div>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
